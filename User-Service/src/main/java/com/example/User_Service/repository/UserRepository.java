27be6d8f53f0328869dbde60c7ab38bfc265df52
Aleksander <vanchukov2018@yandex.ru>
2025-06-30 10:50:26 +0300

    Удаление лишнего

--- User-Service/src/main/java/com/example/User_Service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.User_Service.repository;
-
-import com.example.User_Service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- User-Service/src/main/resources/Purchase_history.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE user_purchase_history (
-    user_id BIGINT NOT NULL,
-    purchase VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
